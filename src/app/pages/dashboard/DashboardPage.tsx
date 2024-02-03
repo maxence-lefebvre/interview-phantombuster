cf1f41de016c9c6ac261e7ed027233c21a6046fb
mlefebvre-amalia <maxence@amalia.io>
2024-02-03 16:08:08 +0100

    feat: better responsive classes

--- src/app/pages/dashboard/DashboardPage.tsx
+++ src/app/pages/dashboard/DashboardPage.tsx
@@ -9,7 +9,7 @@ import {
 export const DashboardPage = () => {
   return (
     <Fragment>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col justify-between gap-4 md:flex-row md:items-center">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         <div className="flex items-center gap-4">
           <PhantomCount />
