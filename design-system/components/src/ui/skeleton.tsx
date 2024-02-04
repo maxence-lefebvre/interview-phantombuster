1256fa7ec409a1a26408f7fbc93e2297ade43c0d
mlefebvre-amalia <maxence@amalia.io>
2024-02-04 12:26:47 +0100

    refactor: prefer CPropsWithoutRef over HTMLAttributes

--- design-system/components/src/ui/skeleton.tsx
+++ design-system/components/src/ui/skeleton.tsx
@@ -1,9 +1,8 @@
+import { ComponentPropsWithoutRef } from 'react';
+
 import { cn } from '@phantombuster/design-system/core';
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: ComponentPropsWithoutRef<'div'>) {
   return (
     <div
       className={cn('animate-pulse rounded-md bg-primary/10', className)}
