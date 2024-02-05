2dd8013123d19bd30c3053cfb6e157c7916c626e
mlefebvre-amalia <maxence@amalia.io>
2024-02-05 10:40:04 +0100

    test: add some unit test with vitest for app

---
+++ src/app/layout/DefaultErrorPage.spec.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import { useRouteError } from 'react-router-dom';
+import { vi } from 'vitest';
+
+import { DefaultErrorPage } from './DefaultErrorPage';
+
+vi.mock('react-router-dom');
+
+const useRouteErrorMock = vi.mocked(useRouteError);
+
+describe('DefaultErrorPage', () => {
+  describe('when an error occurs', () => {
+    it('should display status text over the error message', () => {
+      useRouteErrorMock.mockReturnValue({
+        statusText: 'Internal Server Error',
+        message: 'This wont be displayed!',
+      });
+
+      render(<DefaultErrorPage />);
+
+      expect(screen.getByText('Internal Server Error')).toBeInTheDocument();
+      expect(
+        screen.queryByText('This wont be displayed'),
+      ).not.toBeInTheDocument();
+    });
+
+    it('should display the error message if there is no status text', () => {
+      useRouteErrorMock.mockReturnValue({
+        message: 'The server is on fire!',
+      });
+
+      render(<DefaultErrorPage />);
+
+      expect(screen.getByText('The server is on fire!')).toBeInTheDocument();
+    });
+  });
+});
